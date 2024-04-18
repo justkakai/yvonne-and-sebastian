e60ac6b1c3226456d70817e247af0d11a6026b0f
Kakai Wapenyi <kakai.wapenyi@everyone-energy.de>
2024-04-19 00:46:13 +0200

    fix padding issue in WhatToKnow

--- src/components/WhatToKnow.tsx
+++ src/components/WhatToKnow.tsx
@@ -48,7 +48,7 @@ function WhatToKnow() {
 
 	return (
 		<Box mt={12} px={[0, 4, 16, 24, 40]}>
-			<Flex direction={'column'} gap={10} px={8} lineHeight={8} fontSize={'lg'} color={'#4a4a4a'}>
+			<Flex direction={'column'} gap={10} lineHeight={8} fontSize={'lg'} color={'#4a4a4a'}>
 				{dosAndDonts.map(section => (
 					<Box key={section.id}>
 						<Heading size='md' mb={4}>{section.title}</Heading>
